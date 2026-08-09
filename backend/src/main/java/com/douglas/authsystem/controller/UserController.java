29d6e1a91db9ce7ca8046649f5934d4bc7348324
Douglas <douglasribowski17@gmail.com>
2026-08-09 20:42:56 -0300

    Finalizando o backend

--- backend/src/main/java/com/douglas/authsystem/controller/UserController.java
+++ backend/src/main/java/com/douglas/authsystem/controller/UserController.java
@@ -5,15 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Olá Douglas! Minha API Spring Boot está funcionando!";
-    }
-
-    @GetMapping("/me")
-    public String me(Authentication authentication) {
+    @GetMapping("/user/me")
+    public String getCurrentUser(Authentication authentication) {
 
         return "Usuário autenticado: " + authentication.getName();
     }
